3e69ca09a9549160aaa3476cc335f79993a43f1b
Anacarolina1002 <carolquaglioto@outlook.com>
2020-11-11 16:33:32 -0200

    :star: fix: atividade 1

--- atividade1.py
+++ atividade1.py
@@ -3,19 +3,14 @@
 #input() do python. So que fazendo a validação para aceitar apenas um valor númerico.
 #Ex: inteiro, real. Obs: Se o usuario não digitar o valor, 
 #quero que ele atribua o valor 0 a aquela variável.
+import re
 def leiaInt():
     a=(input("Insira um número:"))
-    stringS(a)
-    if (a == ""):
+    if re.match('^\d+$', a) or re.match('^\d+\.\d+$', a):
+        print("Valor digitado:",a)
+    elif (a == ""):
         a=0
         print("Campo em branco, valor",a,"atribuído.")
     else:
-        print("Valor digitado igual a:",a)
-
-def stringS(valor):
-    try:
-        float(valor)
-    except (ValueError):
-        print("Valor não pôde ser aceito.")
-        SystemExit
+        print("O valor inserido é uma string, inválido!")
 leiaInt()
